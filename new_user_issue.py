c0754a0d3a05ec10085ddfc0d26c76d03eca8d61
Clifford Snow <clifford@snowandsnow.us>
2017-04-15 15:55:18 -0700

    added script to create github issues

---
+++ new_user_issue.py
@@ -0,0 +1,26 @@
+import sys
+from github import Github
+
+token_file = "/home/clifford/.gissues/token.txt"
+user = 'cliffordsnow'
+user_repo = 'newUsers'
+
+t = open(token_file,"r")
+token = str(t.read())[:-1]
+
+g = Github(token)
+
+user = g.get_user(user)
+repo = user.get_repo(user_repo)
+
+changeset = 'Changeset = https://osm.org/changeset/' + str(sys.argv[1])
+user = str(sys.argv[2]) +' https://osm.org/user/' + str(sys.argv[2]).replace(" ","%20")
+comment = 'Comment: ' + str(sys.argv[3])
+location = str(sys.argv[4])
+
+title = str(sys.argv[2]) + " just started editing"
+body = changeset + '\n' + user + '\n' + comment + '\nNear: ' + location + '\n'
+
+newissue = repo.create_issue(title, body)
+
+
